91249eb9c9f677b106d76fb7e0a86f24c74e947f
nuelxelence <nuelxelence@gmail.com>
2022-08-02 16:06:31 +0100

    allowed host

--- blog/settings.py
+++ blog/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = '@@i-o8lc6a6+cayz)ayjcjdig7e6wr!hk8(kn=wnsd+b7up@5v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nuelblog.herokuapp.com','127.0.0.1']
 
 
 # Application definition
